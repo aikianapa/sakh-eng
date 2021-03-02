a78c66d958129f56ddd113e5778715cc9369f2ff
Blacker <oleg_frolov@mail.ru>
2021-03-02 18:42:47 +0300

    update

---
+++ _loader.inc.php
@@ -0,0 +1,24 @@
+
+<div class="loader">
+    <div>
+        <p>
+            <img src="assets/img/icons/header_logo.svg" alt="logotype" class="header__logo">
+        </p>
+        <br>
+        <div class="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    </div>
+</div>
