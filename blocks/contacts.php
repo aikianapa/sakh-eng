74dae68ac25c1b845e97b93cb3e692a89a807852
Oleg Frolov <oleg_frolov@mail.ru>
2021-11-30 16:54:11 +0300

    contacts

--- blocks/contacts.php
+++ blocks/contacts.php
@@ -8,207 +8,38 @@
                             <div class="col-lg-5">
                                 <div class="contacts__info">
                                     <h1 class="contacts__title page-title">
-                                        Контакты
+                                        {{_parent.header}}
                                     </h1>
                                     <div id="accordion">
+                                        
+                                        <wb-foreach wb="from=divisions">
+                                        <wb-var collapse="show" wb-if="'{{_idx}}'=='0'" else="" />
                                         <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading1">
+                                            <div class="contacts-card-header" id="heading{{_ndx}}">
                                                 <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse1" aria-expanded="false"
-                                                    aria-controls="collapse1">
-                                                    Местонахождение
+                                                    data-target="#collapse{{_ndx}}" aria-expanded="false"
+                                                    aria-controls="collapse{{_ndx}}">
+                                                    {{div_name}}
                                                 </a>
                                             </div>
-                                            <div id="collapse1" class="collapse show" aria-labelledby="heading1"
-                                                data-parent="#accordion">
+                                            <div id="collapse{{_ndx}}" class="collapse {{_var.collapse}}" aria-labelledby="heading{{_ndx}}" data-parent="#accordion">
                                                 <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
+                                                    <p class="contacts__contact" data-gpos="{{div_gpos1}}">
+                                                        {{div_addr1}}
                                                     </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
+                                                    <p class="contacts__contact" data-gpos="{{div_gpos2}}">
+                                                        {{div_addr2}}
                                                     </p>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading2">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse2" aria-expanded="false"
-                                                    aria-controls="collapse2">
-                                                    Приемная
-                                                </a>
-                                            </div>
-                                            <div id="collapse2" class="collapse" aria-labelledby="heading2"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading3">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse3" aria-expanded="false"
-                                                    aria-controls="collapse3">
-                                                    Отдел продаж г. Южно-Сахалинск
-                                                </a>
-                                            </div>
-                                            <div id="collapse3" class="collapse" aria-labelledby="heading3"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading4">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse4" aria-expanded="false"
-                                                    aria-controls="collapse4">
-                                                    Отдел продаж г. Хабаровск
-                                                </a>
-                                            </div>
-                                            <div id="collapse4" class="collapse" aria-labelledby="heading4"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading5">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse5" aria-expanded="false"
-                                                    aria-controls="collapse5">
-                                                    Лифтовое подъемное оборудование
-                                                </a>
-                                            </div>
-                                            <div id="collapse5" class="collapse" aria-labelledby="heading5"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading6">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse6" aria-expanded="false"
-                                                    aria-controls="collapse6">
-                                                    Испытательная лаборатория
-                                                </a>
-                                            </div>
-                                            <div id="collapse6" class="collapse" aria-labelledby="heading6"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading7">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse7" aria-expanded="false"
-                                                    aria-controls="collapse7">
-                                                    Отдел закупок
-                                                </a>
-                                            </div>
-                                            <div id="collapse7" class="collapse" aria-labelledby="heading7"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="contacts-card">
-                                            <div class="contacts-card-header" id="heading8">
-                                                <a class="contacts__subtitle collapsed" data-toggle="collapse"
-                                                    data-target="#collapse8" aria-expanded="false"
-                                                    aria-controls="collapse8">
-                                                    Отдел кадров
-                                                </a>
-                                            </div>
-                                            <div id="collapse8" class="collapse" aria-labelledby="heading8"
-                                                data-parent="#accordion">
-                                                <div class="contacts__descr">
-                                                    <p class="contacts__contact">
-                                                        г. Южно-Сахалинск, ул. Ленина, д. 69.
-
-                                                        office@sakh-eng.ru
-                                                    </p>
-                                                    <p class="contacts__contact">
-                                                        г. Хабаровск, ул. Пионерская, 1е, оф. 509, 5 этаж.
-
-                                                        sales.khv@sakh-eng.ru
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        </wb-foreach>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-lg-7">
                                 <div class="contacts__map" id="ymap">
-                                    <!--iframe src="https://yandex.ru/map-widget/v1/-/CCUQQTRZgD" width="100%" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe-->
+
                                 </div>
                             </div>
                         </div>
@@ -219,7 +50,7 @@
         </div>
     </div>
 
-    <script src="https://api-maps.yandex.ru/2.1/?apikey=1a2c9fab-5efe-4df5-9b87-4dab6b652569&amp;lang=ru_RU"></script>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=1a2c9fab-5efe-4df5-9b87-4dab6b652569&lang=ru_RU"></script>
     <script>
     ymaps.ready(init);
 
@@ -253,8 +84,17 @@
             }));
 
         $('p.contacts__contact').on('click', function() {
-            let i = $(this).index();
-            myMap.setCenter(pos[i]);
+            let addr = $(this).text();
+            let gpos;
+            eval("gpos = ["+$(this).data('gpos')+"]");
+            myMap.geoObjects.removeAll();
+            myMap.geoObjects.add(new ymaps.Placemark(gpos, {
+                balloonContent: addr
+            }, {
+                preset: 'islands#dotIcon',
+                iconColor: '#0095b6'
+            }));
+            myMap.setCenter(gpos);
 
         });
 
@@ -267,4 +107,23 @@
     <div>
         <wb-module wb="module=yonger&mode=edit&block=common.inc" />
     </div>
+    <wb-multiinput name="divisions">
+        <div class="col-12 mb-1">
+            <input class="form-control tx-semibold" type="text" name="div_name" placeholder="Наименование">
+        </div>
+
+                <div class="col-8 mb-1">
+                    <input class="form-control" type="text" name="div_addr1" placeholder="Адрес в Южно-Сахалинске">
+                </div>
+                <div class="col-4 mb-1">
+                    <input class="form-control" type="text" name="div_gpos1" placeholder="Геопозиция">
+                </div>
+                <div class="col-8 mb-1">
+                    <input class="form-control" type="text" name="div_addr2" placeholder="Адрес в Хабаровске">
+                </div>
+                <div class="col-4 mb-1">
+                    <input class="form-control" type="text" name="div_gpos2" placeholder="Геопозиция">
+                </div>
+
+    </wb-multiinput>
 </edit>
